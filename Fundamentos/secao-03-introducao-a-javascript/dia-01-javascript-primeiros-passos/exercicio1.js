76ed9b4e8626c72f5eea6155da3a258943f0bdac
Fernando Fachin <fernandofachinrm@gmail.com>
2023-01-23 17:31:20 -0400

    exercicio 1

---
+++ Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,19 @@
+
+const a = 5;
+const b = 20;
+
+adicao = a + b;
+console.log(adicao)
+
+subtracao = a - b;
+console.log(subtracao)
+
+multiplicacao = a * b;
+console.log(multiplicacao)
+
+divisao = a / b;
+console.log(divisao)
+
+modulo = a**b;
+console.log(modulo)
+
