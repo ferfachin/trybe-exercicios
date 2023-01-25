fbe550dc3aa7f13b90f486c88058521a0ae12f8b
Fernando Fachin <fernandofachinrm@gmail.com>
2023-01-25 14:54:21 -0400

    Primeiro commit dia 03 JavaScript

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -1,4 +1,9 @@
+//Obtenha o valor “Serviços” do array menu:
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+//Procure o índice do valor “Portfólio” do array menu:
+
