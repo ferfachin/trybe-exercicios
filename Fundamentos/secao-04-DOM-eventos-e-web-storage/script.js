9d361e6b4cd472b3d8dd44c07194ed4ac053c4ab
ferfachin <fernandofachinrm@gmail.com>
2023-02-06 12:44:14 -0400

    Entendendo o Dom no primeiro exercicio

---
+++ Fundamentos/secao-04-DOM-eventos-e-web-storage/script.js
@@ -0,0 +1,5 @@
+  const tittle = document.getElementById("page-title").innerText = "Questão de tempo"
+  const paragraph = document.getElementById("paragraph");
+  paragraph.style.color = "blue";
+  const subtitle = document.getElementById("subtitle").innerText = "Porque questão de tempo é tão bom?"
+  const subParagraph = document.getElementById("second-paragraph").innerText = "Questão de tempo é o melhor, ele trás um historia linda e com reviravoltas perfeitas"
