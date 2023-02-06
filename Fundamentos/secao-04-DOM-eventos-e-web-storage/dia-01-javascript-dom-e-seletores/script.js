a217fd3405d4305dcbb7df56078e03d2f8469ac0
ferfachin <fernandofachinrm@gmail.com>
2023-02-06 17:15:20 -0400

    Exercicios

--- Fundamentos/secao-04-DOM-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/secao-04-DOM-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,5 +1,18 @@
-  const tittle = document.getElementById("page-title").innerText = "Questão de tempo"
-  const paragraph = document.getElementById("paragraph");
-  paragraph.style.color = "blue";
-  const subtitle = document.getElementById("subtitle").innerText = "Porque questão de tempo é tão bom?"
-  const subParagraph = document.getElementById("second-paragraph").innerText = "Questão de tempo é o melhor, ele trás um historia linda e com reviravoltas perfeitas"
+/* const tittle = document.getElementById("page-title").innerText = "Questão de tempo"
+  
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "blue";
+  
+const subtitle = document.getElementById("subtitle").innerText = "Porque questão de tempo é tão bom?"
+  
+const subParagraph = document.getElementById("second-paragraph").innerText = "Questão de tempo é o melhor, ele trás um historia linda e com reviravoltas perfeitas"
+*/
+
+//let tagElemente = document.getElementsByTagName("p")[0].innerText = "VAI AVIÃO";
+
+//let classElemen = document.getElementsByClassName("subtitle")[0].innerText = "VOLTA AVIÃO";
+//subtitle.style.color ="pink";
+
+
+
+ 
